168faa583de7912a9b729e5fcbaf4ddf57119f86
moz <moz@local>
2018-03-10 19:54:54 +0100

    more updates

--- ConsoleTemplate/cardturner.py
+++ ConsoleTemplate/cardturner.py
@@ -4,8 +4,10 @@ from curses import KEY_RIGHT, KEY_LEFT, KEY_DOWN, KEY_UP
 
 class BoardValues(object):
     spacer = (2, 1)
-    card_size = (5, 5)
-    offset = (2, 1)
+    card_size = (6, 7)
+    offset = (3, 1)
+    rows = 2
+    cols = 3
 
 
 class SelectedMarker(object):
@@ -23,12 +25,27 @@ class SelectedMarker(object):
         self.col = col
 
     def render(self):
+        x = self.geo.card_size[0] * self.col \
+            + self.geo.spacer[0] * self.col \
+            + self.geo.offset[0] - 1
+        y = (self.geo.card_size[1] + 1) * self.row + \
+            self.geo.spacer[1] * self.row \
+            + self.geo.offset[1]
+        self.window.addstr(y, x, 'Y')
         return
 
     def update(self):
         return
 
     def update_direction(self, direction):
+        if direction == KEY_LEFT:
+            self.col = (self.col - 1) % (self.geo.cols)
+            return
+
+        if direction == KEY_RIGHT:
+            self.col = (self.col + 1) % (self.geo.cols)
+            return
+
         return
 
     def reset(self):
@@ -37,7 +54,7 @@ class SelectedMarker(object):
 
 class Card(object):
     def __init__(self, window, row, col, geometry):
-        self.card_size = (5, 5)
+        self.card_size = geometry.card_size
         self.window = window
         self.row = row
         self.col = col
@@ -68,11 +85,11 @@ class Card(object):
 
 
 class Board(object):
-    def __init__(self, window, row_count, col_count, geometry):
+    def __init__(self, window, geometry):
         self.card_size = (5, 5)
         self.window = window
-        self.rows = row_count
-        self.cols = col_count
+        self.rows = geometry.rows
+        self.cols = geometry.cols
 
         self.geo = geometry
         self.cards = []
@@ -109,10 +126,12 @@ def run_game(width, height):
     game = GameBoard(width, height)
     window = game.get_window()
 
-    game.add_game_object(Board(window, 2, 2, BoardValues()))
+    bv = BoardValues()
+    game.add_game_object(Board(window, bv))
+    game.add_game_object(SelectedMarker(window, bv))
 
     game.start()
 
 
 if __name__ == '__main__':
-    run_game(45, 50)
+    run_game(45, 20)
