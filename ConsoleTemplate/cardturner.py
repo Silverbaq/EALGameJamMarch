351ad0f65b1ba4a79135cb8ad38645b19dd4dd93
moz <moz@local>
2018-03-10 23:37:27 +0100

    it works!

--- ConsoleTemplate/cardturner.py
+++ ConsoleTemplate/cardturner.py
@@ -42,52 +42,62 @@ class SelectedMarker(object):
 
 
 class Card(object):
-    def __init__(self, window, row, col, geometry, type):
-        self.card_size = geometry.card_size
-        self.window = window
-        self.row = row
-        self.col = col
-        self.geo = geometry
-        self.turned = False
-        self.disabled = False
-        self.type = str(type)
+    def __init__(self, window, row, col, geometry, card_type):
+        self._window = window
+        self._row = row
+        self._col = col
+        self._geo = geometry
+        self._show_front = False
+        self._disabled = False
+        self._card_type = str(card_type)
 
-    def set_turned(self, state):
-        self.turned = state
+    @property
+    def card_type(self):
+        return self._card_type
 
-    def is_turned(self):
-        return self.turned
-
-    def get_pos(self):
-        return self.row, self.col
-
-    def render_back(self):
-        for c in range(self.card_size[0]):
-            for r in range(self.card_size[1]):
-                x = self.geo.card_size[0] * self.col \
-                    + self.geo.spacer[0] * self.col \
-                    + self.geo.offset[0] + c
-                y = self.geo.card_size[1] * self.row + \
-                    self.geo.spacer[1] * self.row \
-                    + self.geo.offset[1] + r
-                self.window.addstr(y, x, 'X')
-
-    def render_front(self):
-        for c in range(self.card_size[0]):
-            for r in range(self.card_size[1]):
-                x = self.geo.card_size[0] * self.col \
-                    + self.geo.spacer[0] * self.col \
-                    + self.geo.offset[0] + c
-                y = self.geo.card_size[1] * self.row + \
-                    self.geo.spacer[1] * self.row \
-                    + self.geo.offset[1] + r
-                self.window.addstr(y, x, self.type)
-
-    def render(self):
-        if self.disabled:
+    def set_show_front(self, state):
+        if self.is_disabled():
+            self._show_front = False
             return
 
-        if not self.turned:
+        self._show_front = state
+
+    def is_show_front(self):
+        return self._show_front
+
+    def is_disabled(self):
+        return self._disabled
+
+    def set_disabled(self, state):
+        self._disabled = state
+
+    def render_back(self):
+        for c in range(self._geo.card_size[0]):
+            for r in range(self._geo.card_size[1]):
+                x = self._geo.card_size[0] * self._col \
+                    + self._geo.spacer[0] * self._col \
+                    + self._geo.offset[0] + c
+                y = self._geo.card_size[1] * self._row + \
+                    self._geo.spacer[1] * self._row \
+                    + self._geo.offset[1] + r
+                self._window.addstr(y, x, 'X')
+
+    def render_front(self):
+        for c in range(self._geo.card_size[0]):
+            for r in range(self._geo.card_size[1]):
+                x = self._geo.card_size[0] * self._col \
+                    + self._geo.spacer[0] * self._col \
+                    + self._geo.offset[0] + c
+                y = self._geo.card_size[1] * self._row + \
+                    self._geo.spacer[1] * self._row \
+                    + self._geo.offset[1] + r
+                self._window.addstr(y, x, self._card_type)
+
+    def render(self):
+        if self._disabled:
+            return
+
+        if not self.is_show_front():
             self.render_back()
         else:
             self.render_front()
@@ -97,8 +107,8 @@ class Card(object):
 
     def update_direction(self, direction):
         if direction == 10:  # enter key
-            if (self.geo.current_row == self.row) and (self.geo.current_col == self.col):
-                self.turned = not self.turned
+            if (self._geo.current_row == self._row) and (self._geo.current_col == self._col):
+                self._show_front = not self.is_show_front()
 
         return
 
@@ -122,7 +132,7 @@ class Board(object):
 
     def init_cards(self, window):
         card_array = []
-        for i in range((self.cols * self.rows)//2):
+        for i in range((self.cols * self.rows) // 2):
             card_array.append(i)
             card_array.append(i)
         random.shuffle(card_array)
@@ -130,10 +140,10 @@ class Board(object):
         for i in range(self.rows):
             for j in range(self.cols):
                 self.cards.append(Card(window, i, j, self.geo,
-                                       card_array[i * self.cols + j]))
+                                       card_array[self.get_card_number(i, j)]))
 
     def get_card_number(self, row, col):
-        return row * self.rows + col
+        return row * self.cols + col
 
     def render(self):
         for card in self.cards:
@@ -142,10 +152,35 @@ class Board(object):
     def update(self):
         if self.is_waiting:
             if datetime.datetime.now() > self.wait_until_time:
-                for c in self.cards:
-                    c.set_turned(False)
                 self.is_waiting = False
+                self.compare_turned()
+                for c in self.cards:
+                    c.set_show_front(False)
         return
+
+    def compare_turned(self):
+        turned = None
+#        for c in range(len(self.cards)):
+        for c in self.cards:
+            if c.is_show_front():
+                if not turned:
+                    turned = c
+                else:
+                    if turned.card_type == c.card_type:
+                        c.set_disabled(True)
+                        c.set_show_front(True)
+                        turned.set_disabled(True)
+                        turned.set_show_front(True)
+#        for c in self.cards:
+#            if self.cards[c].is_show_front():
+#                if not turned:
+#                    turned = c
+#                else:
+#                    if self.cards[turned].card_type == self.cards[c].card_type:
+#                        self.cards[c].set_disabled(True)
+#                        self.cards[c].set_show_front(True)
+#                        self.cards[turned].set_disabled(True)
+#                        self.cards[turned].set_show_front(True)
 
     def update_direction(self, direction):
         row, col = self.geo.current_row, self.geo.current_col
@@ -174,7 +209,7 @@ class Board(object):
         return
 
     def check_turned(self):
-        sum_turned = sum(1 for c in self.cards if c.is_turned())
+        sum_turned = sum(1 for c in self.cards if c.is_show_front())
         if sum_turned > 1:
             self.set_wait_state()
 
