cbcce8ae6aa9c32cccbfe330155612546b42ef72
moz <moz@local>
2018-03-10 21:14:44 +0100

    added use of enter key

--- ConsoleTemplate/cardturner.py
+++ ConsoleTemplate/cardturner.py
@@ -1,35 +1,30 @@
 from MyGameEngine import GameBoard
-from curses import KEY_RIGHT, KEY_LEFT, KEY_DOWN, KEY_UP
+from curses import KEY_RIGHT, KEY_LEFT, KEY_DOWN, KEY_UP, KEY_ENTER
 
 
 class BoardValues(object):
     spacer = (2, 1)
-    card_size = (6, 7)
+    card_size = (7, 7)
     offset = (3, 1)
     rows = 2
     cols = 3
+
+    def __init__(self):
+        self.current_row = 0
+        self.current_col = 0
 
 
 class SelectedMarker(object):
     def __init__(self, window, geometry):
         self.window = window
         self.geo = geometry
-        self.row = 0
-        self.col = 0
-
-    def get_pos(self):
-        return self.row, self.col
-
-    def set_pos(self, row, col):
-        self.row = row
-        self.col = col
 
     def render(self):
-        x = self.geo.card_size[0] * self.col \
-            + self.geo.spacer[0] * self.col \
+        x = self.geo.card_size[0] * self.geo.current_col \
+            + self.geo.spacer[0] * self.geo.current_col \
             + self.geo.offset[0] - 1
-        y = (self.geo.card_size[1] + 1) * self.row + \
-            self.geo.spacer[1] * self.row \
+        y = (self.geo.card_size[1] + 1) * self.geo.current_row + \
+            self.geo.spacer[1] * self.geo.current_row \
             + self.geo.offset[1]
         self.window.addstr(y, x, 'Y')
         return
@@ -38,14 +33,6 @@ class SelectedMarker(object):
         return
 
     def update_direction(self, direction):
-        if direction == KEY_LEFT:
-            self.col = (self.col - 1) % (self.geo.cols)
-            return
-
-        if direction == KEY_RIGHT:
-            self.col = (self.col + 1) % (self.geo.cols)
-            return
-
         return
 
     def reset(self):
@@ -59,11 +46,12 @@ class Card(object):
         self.row = row
         self.col = col
         self.geo = geometry
+        self.turned = False
 
     def get_pos(self):
         return self.row, self.col
 
-    def render(self):
+    def render_back(self):
         for c in range(self.card_size[0]):
             for r in range(self.card_size[1]):
                 x = self.geo.card_size[0] * self.col \
@@ -74,10 +62,31 @@ class Card(object):
                     + self.geo.offset[1] + r
                 self.window.addstr(y, x, 'X')
 
+    def render_front(self):
+        for c in range(self.card_size[0]):
+            for r in range(self.card_size[1]):
+                x = self.geo.card_size[0] * self.col \
+                    + self.geo.spacer[0] * self.col \
+                    + self.geo.offset[0] + c
+                y = self.geo.card_size[1] * self.row + \
+                    self.geo.spacer[1] * self.row \
+                    + self.geo.offset[1] + r
+                self.window.addstr(y, x, '0')
+
+    def render(self):
+        if self.turned:
+            self.render_back()
+        else:
+            self.render_front()
+
     def update(self):
         return
 
     def update_direction(self, direction):
+        if direction == 10: # enter key
+            if (self.geo.current_row == self.row) and (self.geo.current_col == self.col):
+                self.turned = not self.turned
+
         return
 
     def reset(self):
@@ -98,8 +107,6 @@ class Board(object):
             for j in range(self.cols):
                 self.cards.append(Card(window, i, j, self.geo))
 
-        self.current_card = self.get_card_number(0, 0)
-
     def get_card_number(self, row, col):
         return row * self.rows + col
 
@@ -111,10 +118,26 @@ class Board(object):
         return
 
     def update_direction(self, direction):
-        #        if direction == KEY_LEFT:
-        #            r, c = self.cards[self.current_card].get
-        #            pos()
-        #
+        for c in self.cards:
+            c.update_direction(direction)
+
+        row, col = self.geo.current_row, self.geo.current_col
+        if direction == KEY_LEFT:
+            self.geo.current_col = (col - 1) % self.geo.cols
+            return
+
+        if direction == KEY_RIGHT:
+            self.geo.current_col = (col + 1) % self.geo.cols
+            return
+
+        if direction == KEY_UP:
+            self.geo.current_row = (row - 1) % self.geo.rows
+            return
+
+        if direction == KEY_DOWN:
+            self.geo.current_row = (row + 1) % self.geo.rows
+            return
+
         return
 
     def reset(self):
